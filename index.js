a28a6c99d373e82a4313ff4e83558ae6b9376e95
MikolajZamojski <mikolaj.zamojski2004@gmail.com>
2022-09-13 21:54:17 +0200

    db integration

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
-
-const port = 3000
+const dbo = require('./db/conn');
+require('dotenv').config()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const dbConnect = dbo.getDb();
+  dbConnect
+    .collection('Posts')
+    .find({})
+    .limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings!');
+      } else {
+        res.json(result);
+      }
+    });
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+dbo.connectToServer(function (err) {
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+  })
+});
