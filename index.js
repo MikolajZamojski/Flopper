f564ecda6ffd4f628daf6204d9777efa48042363
MikolajZamojski <mikolaj.zamojski2004@gmail.com>
2022-09-13 22:44:09 +0200

    Test2

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ require('dotenv').config()
 
 app.get('/', (req, res) => {
   const dbConnect = dbo.getDb();
-  dbConnect
-    .collection('Posts')
-    .find({})
-    .limit(50)
-    .toArray(function (err, result) {
-      if (err) {
-        res.status(400).send('Error fetching listings!');
-      } else {
-        res.json(result);
-      }
-    });
+  // dbConnect
+  //   .collection('Posts')
+  //   .find({})
+  //   .limit(50)
+  //   .toArray(function (err, result) {
+  //     if (err) {
+  //       res.status(400).send('Error fetching listings!');
+  //     } else {
+  //       res.json(result);
+  //     }
+  //   });
+  res.send('Dzieki wiksze pozdrawiam Cie')
 })
 
 dbo.connectToServer(function (err) {
