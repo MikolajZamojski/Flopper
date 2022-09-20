7e23234e16263018cabca6cbaf2b5ba7965c0268
MikolajZamojski <mikolaj.zamojski2004@gmail.com>
2022-09-20 21:52:38 +0200

    Post pfp authorization fix

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,12 @@ const router = express.Router()
 const { upload } = require("../multer/multer")
 const fs = require('fs')
 
-router.post('/:userId/pfp', upload.single('avatar'), async (req, res) => {
-  if(req.params.userId === req.userId) {
-    const updateResult = (await req.dbConnect.collection("Users").findOneAndUpdate({_id: req.userId}, {$set : {"pfp-filename": req.hashedFileName}}, {upsert: true, projection: {"pfp-filename": 1, _id: 0}})).value["pfp-filename"];
-    if(updateResult) {
-      await fs.rm('uploads/' + updateResult.split('').slice(0, 3).join("/") + "/" + updateResult, (err) => {if(err) console.log(err)})
-    }
-    res.sendStatus(201);
+router.post('/:userId/pfp', (req, res, next) => {req.params.userId === req.userId ? next() : res.sendStatus(403)}, upload.single('avatar'), async (req, res) => {
+  const updateResult = (await req.dbConnect.collection("Users").findOneAndUpdate({_id: req.userId}, {$set : {"pfp-filename": req.hashedFileName}}, {upsert: true, projection: {"pfp-filename": 1, _id: 0}})).value["pfp-filename"];
+  if(updateResult) {
+    await fs.rm('uploads/' + updateResult.split('').slice(0, 3).join("/") + "/" + updateResult, (err) => {if(err) console.log(err)})
   }
-  else {
-    res.sendStatus(403)
-  }
+  res.sendStatus(201);
 })
 
 module.exports = router
