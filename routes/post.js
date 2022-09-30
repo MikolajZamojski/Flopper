58f1de4d4716c1fcf0d89aaaf9337e052735ef6b
MikolajZamojski <84736386+MikolajZamojski@users.noreply.github.com>
2022-09-30 13:01:42 +0200

    Comments managing

--- routes/post.js
+++ routes/post.js
@@ -28,4 +28,44 @@ router.put('/:postId/like', authenticateToken, async(req, res) => {
   return res.sendStatus(201);
 })
 
+router.post('/:postId/c', authenticateToken, async(req, res) => {
+  const post = await req.dbConnect.collection("Posts").findOne({_id: req.params.postId});
+  if(post === null) {
+    return res.status(400).json({err: "Post doesn't exist!"});
+  }
+  const {text} = req.body;
+  const commentId = crypto.randomUUID().replaceAll('-', '');
+  await req.dbConnect.collection("Comments").insertOne({_id: commentId, text: text, post: post._id, author: req.userId, date: new Date()});
+  res.sendStatus(201);
+})
+
+// add partial index
+router.put('/:postId/c/:commentId/like', authenticateToken, async(req, res) => {
+  const comment = await req.dbConnect.collection("Comments").findOne({post: req.params.postId, _id: req.params.commentId});
+  if(comment === null) {
+    return res.status(400).json({err: "Comment doesn't exist!"});
+  }
+  const likeResult = await req.dbConnect.collection("CommentsLikes").findOne({comment: req.params.commentId, user: req.userId});
+  if(likeResult !== null) {
+    await req.dbConnect.collection("CommentsLikes").deleteOne({_id: likeResult._id});
+    await req.dbConnect.collection("Comments").updateOne({_id: comment._id}, {$inc: {"likes-count": -1}});
+  }
+  else {
+    await req.dbConnect.collection("CommentsLikes").insertOne({comment: comment._id, user: req.userId});
+    await req.dbConnect.collection("Comments").updateOne({_id: comment._id}, {$inc: {"likes-count": 1}}, {upsert: true});
+  }
+  return res.sendStatus(201);
+})
+
+router.post('/:postId/c/:commentId/reply', authenticateToken, async(req, res) => {
+  const comment = await req.dbConnect.collection("Comments").findOne({post: req.params.postId, _id: req.params.commentId});
+  if(comment === null) {
+    return res.status(400).json({err: "Comment doesn't exist!"});
+  }
+  const {text} = req.body;
+  const commentId = crypto.randomUUID().replaceAll('-', '');
+  await req.dbConnect.collection("Comments").insertOne({_id: commentId, text: text, post: req.params.postId, author: req.userId, date: new Date(), "answering-comment": comment._id});
+  res.sendStatus(201);
+})
+
 module.exports = router
