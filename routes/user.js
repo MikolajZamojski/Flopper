42acfb909390a67cd0519c8cda714a30fbf9ca47
MikolajZamojski <84736386+MikolajZamojski@users.noreply.github.com>
2022-12-06 10:24:15 +0100

    ENOENT test

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 const authenticateToken = require('../middlewares/authenticateToken');
 
 router.post('/pfp', authenticateToken, pfpUpload.single('avatar'), async (req, res) => {
+  console.log("f2")
   const updateResult = (await req.dbConnect.collection("Users").findOneAndUpdate({_id: req.userId}, {$set : {"pfp-filename": req.hashedFileName}}, {upsert: true, projection: {"pfp-filename": 1, _id: 0}})).value["pfp-filename"];
   if(updateResult) {
     await fs.rm('public/pfps/' + updateResult.split('').slice(0, 3).join("/") + "/" + updateResult, (err) => {if(err) console.log(err)})
