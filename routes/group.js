578bc9ae31062902e9931d12ec6d605a4e1b646a
MikolajZamojski <mikolaj.zamojski2004@gmail.com>
2023-06-23 00:10:24 +0200

    Group deletion route test

--- routes/group.js
+++ routes/group.js
@@ -168,8 +168,6 @@ router.put('/:groupId/:userId/invite', authenticateToken, authorizeGroupOwnershi
   return res.status(409).json({err: "Already a member"});
 })
 
-router.use(cors({origin: 'https://flopper-client.vercel.app'}))
-
 router.delete('/:groupId/quit', authenticateToken, async(req, res) => {
   const data = await req.dbConnect.collection("Groups").findOne({_id: req.params.groupId});
   if(data === null) {
@@ -216,20 +214,20 @@ router.delete('/:groupId/:userId/kick', authenticateToken, authorizeGroupOwnersh
 
 
 router.delete('/:groupId', authenticateToken, authorizeGroupOwnership, async(req, res) => {
-  await req.dbConnect.collection("Groups").deleteOne({_id: req.params.groupId});
-  await req.dbConnect.collection("GroupsMembers").deleteMany({group: req.params.groupId});
-  const groupPosts = await req.dbConnect.collection("Posts").find({group: req.params.groupId}, {projection: {_id: 1}}).toArray();
-  if(groupPosts) {
-    const groupPostsIds = groupPosts.map(postObj => postObj._id)
-    await req.dbConnect.collection("Posts").deleteMany({group: req.params.groupId});
-    await req.dbConnect.collection("PostsLikes").deleteMany({post: {$in: groupPostsIds}});
-    const deletedComments = await req.dbConnect.collection("Comments").find({post: {$in: groupPostsIds}}, {projection: {_id: 1}}).toArray();
-    if(deletedComments) {
-      const deletedCommentsIds = deletedComments.map(commentObj => commentObj._id);
-      await req.dbConnect.collection("Comments").deleteMany({post: {$in: groupPostsIds}});
-      await req.dbConnect.collection("CommentsLikes").deleteMany({comment: {$in: deletedCommentsIds}});
-    }
-  }
+  // await req.dbConnect.collection("Groups").deleteOne({_id: req.params.groupId});
+  // await req.dbConnect.collection("GroupsMembers").deleteMany({group: req.params.groupId});
+  // const groupPosts = await req.dbConnect.collection("Posts").find({group: req.params.groupId}, {projection: {_id: 1}}).toArray();
+  // if(groupPosts) {
+  //   const groupPostsIds = groupPosts.map(postObj => postObj._id)
+  //   await req.dbConnect.collection("Posts").deleteMany({group: req.params.groupId});
+  //   await req.dbConnect.collection("PostsLikes").deleteMany({post: {$in: groupPostsIds}});
+  //   const deletedComments = await req.dbConnect.collection("Comments").find({post: {$in: groupPostsIds}}, {projection: {_id: 1}}).toArray();
+  //   if(deletedComments) {
+  //     const deletedCommentsIds = deletedComments.map(commentObj => commentObj._id);
+  //     await req.dbConnect.collection("Comments").deleteMany({post: {$in: groupPostsIds}});
+  //     await req.dbConnect.collection("CommentsLikes").deleteMany({comment: {$in: deletedCommentsIds}});
+  //   }
+  // }
   return res.status(200).json({msg: "Group deleted."});
 })
 
